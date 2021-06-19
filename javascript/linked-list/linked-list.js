e02699920a0ab30a289ae6f99a08b85f82cccd2c
Alexander Williams <alex.williams2311@gmail.com>
2021-06-18 19:22:14 -0700

    linkedList

---
+++ javascript/linked-list/linked-list.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(){
+    this.head = null;
+  }
+}
+
+const list = new LinkedList();
+
+console.log(list);
+
+
+
